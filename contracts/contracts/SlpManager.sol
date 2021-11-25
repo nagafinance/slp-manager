5d160ea32cc750f9f9da7b6e30a8a901d8e74775
nuttakit-vy <56815700+NuttakitDW@users.noreply.github.com>
2021-11-25 13:59:29 +0700

    yolo

--- contracts/contracts/SlpManager.sol
+++ contracts/contracts/SlpManager.sol
@@ -15,15 +15,23 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 contract SlpManager is Ownable, ReentrancyGuard, ChainlinkClient {
     using SafeERC20 for IERC20;
+    using Chainlink for Chainlink.Request;
     uint256 constant ONE = 1e18;
     
     struct ScholarInfo {
         address roninAddress;
         address player;
-        address keeper;
         uint256 claimable;
         uint256 percentShare; // 1e18 = 100 %
     }
+
+    /**
+     * Network: Kovan
+     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
+     * Node)
+     * Job ID: d5270d1c311941d0b08bead21fea7747
+     * Fee: 0.1 LINK
+     */
     
     uint256 public percentFee;
     address public devaddr;
@@ -31,7 +39,14 @@ contract SlpManager is Ownable, ReentrancyGuard, ChainlinkClient {
     address public feeAddress;
     uint256 public balance;
     IERC20 public slp;
+
+    //chainlink var
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
     
+    uint256 public claimableTotal;
+
     //Info of each scholar
     ScholarInfo[] public scholarInfo;
     //list of player
@@ -45,14 +60,19 @@ contract SlpManager is Ownable, ReentrancyGuard, ChainlinkClient {
     // Info of recently dailySlp update.
     mapping(address => uint256) lastUpdate;
     // @notice init with a list of recipients
-    constructor(address _token, address _guildMaster, address _keeper, address _feeAddress, uint256 _percentFee) {
+    constructor(address _token, address _guildMaster, address _feeAddress, uint256 _percentFee) {
         slp = IERC20(_token);
         guildMaster = _guildMaster;
         devaddr = msg.sender;
         balance = 0;
         feeAddress = _feeAddress;
         percentFee = _percentFee;
-        keeper = _keeper;
+
+        setPublicChainlinkToken();
+        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
+        jobId = "d5270d1c311941d0b08bead21fea7747";
+        fee = 0.1 * 10 ** 18; // (Varies by network and job)
+
     }
 
     event Deposit(address guildMaster, uint256 amount);
@@ -189,19 +209,62 @@ contract SlpManager is Ownable, ReentrancyGuard, ChainlinkClient {
     
     function updatePaymentBalance(
         uint256 _date,
-        address _roninAddress,
-        uint256 _dailySlp
+        address _roninAddress
         
-    ) public {
+    ) public onlyOwner {
         
         require(lastUpdate[_roninAddress] < _date);
         require(roninList[_roninAddress] == true);
-        require(msg.sender == keepper);
+
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+
+        // Set the URL to perform the GET request on
+        request.add("get", "https://game-api.axie.technology/slp/0x951abd24aca1a033065eb0b6b4e37b6a8a380251");
+
+//        0[	
+// success	true
+// client_id	"0xaf589071ed4e0f4aa081d445b7644ac75cd722c4"
+// item_id	1
+// total	1345
+// blockchain_related	
+// signature	
+// signature	"0x01970f31fc0a80e182d7d82c95451fda23d5e5995ea72b0f253648fe1b5c306f003749866878a41358dc46ab0aa2b64beb31d6964afdbe56ea3666d165fe0530de1c"
+// amount	10362
+// timestamp	1636963731
+// balance	0
+// checkpoint	10362
+// block_number	8488065
+// claimable_total	0
+// last_claimed_item_at	1636963731
+// item	
+// id	1
+// name	"Breeding Potion"
+// description	"Breeding Potion is required to breed two Axies"
+// image_url	""
+// updated_at	1576669291
+// created_at	1576669291
+// update_time	1637821585428
+
+        request.add("path", "0.claimable_total");
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
 
         uint256 id = roninInfo[_roninAddress];
 
-        scholarInfo[id].claimable += (_dailySlp * scholarInfo[i].percentShare) / ONE;
+        sendChainlinkRequestTo(oracle, request, fee);
+
+        uint256 amount = this.claimableTotal();
+
+        scholarInfo[id].claimable += (amount * scholarInfo[id].percentShare) / ONE;
         lastUpdate[_roninAddress] = _date;
+
+    }
+
+    function fulfill(bytes32 _requestId, uint256 _claimableTotal) public recordChainlinkFulfillment(_requestId)
+    {
+        claimableTotal = _claimableTotal;
     }
     
 }
